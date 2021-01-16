656a10a9c00ecbd2388e1035b8edcf29441850d8
zhuanghaixin <649308293@qq.com>
2021-01-16 14:45:03 +0800

    feat(uni-app):新增小程序所需图片，字体，公共样式，配置公共less,使用公共变量，路由配置

--- App.vue
+++ App.vue
@@ -12,6 +12,21 @@
 	}
 </script>
 
-<style>
+<style lang="less">
+	// 为什么这里需要在lang="less"
 	/*每个页面公共css */
+	view,
+	image {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	image {
+		vertical-align: middle;
+	}
+
+	@import url('./static/css/iconfont.css');
+	@import url("./uni.less");
+	@import url("./style/variable.less");
 </style>
--- main.js
+++ main.js
@@ -2,10 +2,18 @@ import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
-
+// 导入网络请求的方法
+import request from './utils/request.js'
+Vue.prototype.$axios = request
+// 引入外部图标
+import './style/iconfont/iconfont.css'
+// import './static/css/iconfont.css';
+// 设置项目类型是app
 App.mpType = 'app'
 
+// 创建一个项目实例
+
 const app = new Vue({
-    ...App
+    ...App    // 将App组件渲染到Vue实例中   
 })
-app.$mount()
+app.$mount()  // 将vue实例挂载到页面中
